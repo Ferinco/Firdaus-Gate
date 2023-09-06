95a9c61fa929f6ff5efa084ab6d1db9d6769e7eb
Ferinco <emmanuelferrum003@gmail.com>
2023-09-06 11:11:43 +0100

    report page designs

--- src/layout/dashboard/Student/resultsPage.js
+++ src/layout/dashboard/Student/resultsPage.js
@@ -1,9 +1,144 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+import { Icon } from "@iconify/react";
 
-function resultsPage() {
+export default function ResultsPage() {
   return (
-    <div>resultsPage</div>
-  )
+    <Wrapper className="p-5">
+      <div className="">
+        <h4>Reports</h4>
+        <p>View reports for each school term</p>
+      </div>
+     <div className="tabs-wrapper py-5 mt-5">
+     <div className="tabs row w-100 p-0 py-5 px-3">
+        <div className="tab ">
+          <div className="tab-right">
+            <div className="icon-div">
+              <Icon icon="icon-park-solid:one-key" className="icon" />
+            </div>
+            <div className="text d-flex flex-column">
+              <h6>1st</h6>
+              <p>1st term reports</p>
+            </div>
+          </div>
+          <div className="tab-left">
+            <Icon icon="icon-park-solid:one-key" className="big-icon" />
+          </div>
+        </div>
+
+        <a href="" download="Adekoya Ismail" className=" tab d-flex flex-row">
+          <div className="tab-right ">
+            <div className="icon-div">
+              <Icon icon="icon-park-solid:two-key" className="icon" />
+            </div>
+            <div className="text d-flex flex-column">
+              <h6>2nd</h6>
+              <p>2nd term reports</p>
+            </div>
+          </div>
+          <div className="tab-left">
+            <Icon icon="icon-park-solid:two-key" className="big-icon" />
+          </div>
+        </a>
+
+        <div className="tab ">
+          <div className="tab-right">
+            <div className="icon-div">
+              <Icon icon="icon-park-solid:three-key" className="icon" />
+            </div>
+            <div className="text d-flex flex-column">
+              <h6>SCHEME</h6>
+              <p>class scheme</p>
+            </div>
+          </div>
+          <div className="tab-left">
+            <Icon icon="icon-park-solid:three-key" className="big-icon" />
+          </div>
+        </div>
+      </div>
+     </div>
+    </Wrapper>
+  );
 }
 
-export default resultsPage
+const Wrapper = styled.div`
+.tabs-wrapper{
+  background-color:white;
+  border-radius: 30px;
+}
+  .tabs {
+    gap: 30px;
+    margin-left: 3px !important;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    .tab {
+      border-radius: 30px;
+      height: 200px;
+      width: 250px;
+      overflow: hidden;
+      justify-content: space-between;
+      display: flex;
+      flex-direction: row;
+      align-items: center !important;
+      padding: 15px;
+      text-decoration: none !important;
+      .tab-right {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        gap: 20px;
+      }
+      .tab-left {
+        margin-right: -30% !important;
+      }
+      .icon-div {
+        padding: 10px;
+        background-color: white;
+        border-radius: 30px;
+        width: 50px;
+      }
+      .icon {
+        font-size: 30px;
+      }
+      &:first-child {
+        background-color: #9ea0e7;
+        color: white;
+        .big-icon {
+          font-size: 150px !important;
+          color: #d2d3e9;
+        }
+        .icon {
+          color: #9ea0e7;
+        }
+      }
+      &:nth-child(2) {
+        background-color: #65655d;
+        color: white;
+        &:hover {
+          transform: scale(1.05);
+        }
+        .big-icon {
+          font-size: 150px !important;
+          color: grey;
+        }
+        .icon {
+          color: #65655d;
+        }
+      }
+      &:last-child {
+        background-color: #9ea0e7;
+        color: white;
+        .big-icon {
+          font-size: 150px !important;
+          color: #d2d3e9;
+        }
+        .icon {
+          color: #9ea0e7;
+        }
+      }
+    }
+  }
+`;
--- src/routes/index.js
+++ src/routes/index.js
@@ -13,7 +13,7 @@ import StudentDashboard from "../layout/dashboard/Student/studentDashboard";
 import Create from "../layout/dashboard/Teacher/create";
 import ProgressPage from "../pages/progressPage";
 import CreateResult from "../pages/teacher/createResult";
-import resultsPage from "../layout/dashboard/Student/resultsPage";
+import ResultsPage from "../layout/dashboard/Student/resultsPage";
 export default function Routes() {
   return useRoutes([
     {
@@ -47,7 +47,7 @@ export default function Routes() {
       element: <StudentDashboardLayout />,
       children: [
         { path: "/student", element: <StudentDashboard /> },
-        { path: "/student/reports", element: <resultsPage /> },
+        { path: "/student/reports", element: <ResultsPage /> },
         // {path: "/teacher/my-class", element: <MyClass/>},
         // {path: "/teacher/results", element: <Results/>}
       ],
