976974b67a56e1a3ee6f75887da0926dc139be97
Ferinco <emmanuelferrum003@gmail.com>
2023-10-03 11:39:55 +0100

    input fields for term calendar

--- src/pages/admin/createCalendar.js
+++ src/pages/admin/createCalendar.js
@@ -7,27 +7,47 @@ import { fetchCurrentTerm } from "../../redux/slices/term";
 export default function CreateCalendar() {
   const dispatch = useDispatch();
   const { currentTerm } = useSelector((state) => state.term);
-  const [weeks, setWeeks] = useState()
+  const [weeks, setWeeks] = useState([]);
   useEffect(() => {
     dispatch(fetchCurrentTerm());
   }, []);
-  console.log(currentTerm)
-  const startDate =new Date(currentTerm.startDate)
-  const endDate =new Date(currentTerm.endDate)
-  console.log(endDate)
-  console.log(startDate)
-  const dateDifference = endDate - startDate
-  console.log(dateDifference)
-  const weeksDifference = Math.ceil(dateDifference / (1000 * 3600 * 24 * 7))
-  console.log(weeksDifference)
+  console.log(currentTerm);
+  const startDate = new Date(currentTerm.startDate);
+  const endDate = new Date(currentTerm.endDate);
+  const dateDifference = endDate - startDate;
+  const weeksDifference = Math.ceil(dateDifference / (1000 * 3600 * 24 * 7));
+  weeks.length = weeksDifference;
+useEffect(()=>{
 
-
+    const initiailWeeks = Array.from({length: weeks.length}, (_, index)=>({
+        value: ""
+    }))
+    setWeeks(initiailWeeks)
+    console.log(initiailWeeks);
+}, [weeksDifference])
+// const handleInputChange = (index, newValue) => {
+//     const updatedWeeks = [...weeks];
+//     updatedWeeks[index].value = newValue;
+//     setWeeks(updatedWeeks);
+//   };
+  console.log(weeks);
   return (
     <div className="p-5">
-        <div>
-            <h4>Term Calendar</h4>
-        </div>
-        <div><p>{weeksDifference}{" "}weeks for {currentTerm.name}</p></div>
+      <div>
+        <h4>Term Calendar</h4>
+      </div>
+      <div>
+        <p>
+          {weeksDifference} weeks for {currentTerm.name}
+        </p>
+      </div>
+      {weeks.map((week, index) => (
+        <input
+          key={index}
+          type="text"
+          value={week.value}
+        />
+      ))}
     </div>
   );
 }
