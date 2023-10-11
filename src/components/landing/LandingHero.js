94ddb42eaef6dacdcccd04440ab33972af334eb9
Ferinco <emmanuelferrum003@gmail.com>
2023-10-11 22:27:24 +0100

    landing page restyle

--- src/components/landing/LandingHero.js
+++ src/components/landing/LandingHero.js
@@ -1,11 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import styled from "styled-components";
 import { Button } from "../custom/Button";
 
 
+const images = [
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  // Add more image URLs as needed
+];
+
+const CarouselContainer = styled.div`
+  perspective: 1200px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100% !important;
+`;
+
+const CarouselWrapper = styled.div`
+  width: 400px;
+  height: 300px;
+  position: relative;
+  transform-style: preserve-3d;
+  margin-left: 100px !important;
+`;
+
+const CarouselSlide = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  backface-visibility: hidden;
+  transition: transform 1s; /* Adjust the duration as needed */
+  transform: rotateY(${(props) => props.rotation}deg) translateZ(200px);
+  &.active {
+    transform: rotateY(0deg) translateZ(200px);
+  }
+  .image {
+    width: 200px;
+    margin-left: -50px;
+    background-color: purple;
+    border-radius: 30px;
+    
+  }
+`;
 export default function LandingHero() {
 
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+  useEffect(() => {
+    // Automatically change the active image every 3 seconds
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <Wrapper className="">
      <div className="container py-5">
@@ -29,9 +85,19 @@ this part should contain a short intro on the school's mission and some other re
           </div>
         </div>
         <div className="col-md-4">
-          <div className="image">
-
-          </div>
+        <CarouselContainer>
+      <CarouselWrapper>
+        {images.map((image, index) => (
+          <CarouselSlide
+            key={index}
+            rotation={(index - currentIndex) * 45}
+            className={index === currentIndex ? 'active' : ''}
+          >
+            <div className="image"></div>
+          </CarouselSlide>
+        ))}
+      </CarouselWrapper>
+    </CarouselContainer>
         </div>
       </div>
      </div>
@@ -71,7 +137,9 @@ align-items: center;
   display: flex;
 justify-content: center;
 }
-
+.col-md-4{
+  
+}
 p{
   font-size: 17px;
 }
@@ -85,8 +153,8 @@ text-align: left;
   }
   .image{
     max-width: 400px;
-    height:500px;
-    border:1px solid red;
+    height:400px;
+    background-color: purple;
     border-radius: 30px;
   }
 `;
