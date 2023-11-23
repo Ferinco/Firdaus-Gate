7866c8b2b3e6ab7871ab19699d14a60c93f6b2c3
Ferinco <emmanuelferrum003@gmail.com>
2023-11-23 19:27:37 +0100

    front page restructure

--- src/components/landing/LandingHero.js
+++ src/components/landing/LandingHero.js
@@ -1,52 +1,41 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Button } from "../custom/Button";
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet";
 
 const images = [
   {
-className: "image image-1",
-content: ""
+    className: "image image-1",
+    content: "",
   },
   {
     className: "image image-2",
-    content: ""
-      },
-      {
-        className: "image image-1",
-        content: ""
-          },
+    content: "",
+  },
+  {
+    className: "image image-1",
+    content: "",
+  },
 
   // Add more image URLs as needed
 ];
 
 const CarouselContainer = styled.div`
-  perspective: 1200px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100% !important;
-@media screen and (max-width: 768px) {
-  display: none;
-  
-}
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
-
-
 const MobileImage = styled.div`
-display: none;
-@media screen and (max-width: 768px) {
-  display: block;
-  width: 100%;
-  height:250px;
-  background-color: purple;
-  
-}
-`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    width: 100%;
+    height: 300px;
+    background-color: purple;
+  }
+`;
 export default function LandingHero() {
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isSticky, setSticky] = useState(false);
 
@@ -55,15 +44,14 @@ export default function LandingHero() {
       setSticky(window.scrollY > 156);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Cleanup the event listener on component unmount
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   useEffect(() => {
-
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
@@ -73,106 +61,116 @@ export default function LandingHero() {
     };
   }, []);
   return (
-    <Wrapper >
-
-     <div className={` h-100 ${isSticky ? 'sticky' : ''}`}>
-     <div className="row h-100">
-        <div className="col-md-4 d-flex left flex-row h-100 p-5">
-          <div className="my-5 d-flex flex-column gap-3 text-div justify-content-center"> 
-            <h1 className="display-3 fw-bolder">
-              Firdaus-Gate Model Schools
-            </h1>
-            <div className="">
-              <p className="">
-              Assalamu Alaikum! Welcome to Firdaus Gate Model Schools, where education meets faith. In our school, we nurture minds, hearts, and character. Explore the enriching blend of academics and Islamic values...
-              </p>
-            </div>
-            <div className="button-group d-flex flex-row">
-              <Button blue>Get started</Button>
-              <span> </span>
-              <Button white>About us</Button>
+    <Wrapper>
+      <div className={` h-100 ${isSticky ? "sticky" : ""}`}>
+        <div className="row h-100">
+          <div className="col-md-4 d-flex left flex-row h-100 p-5">
+            <div className="my-5 d-flex flex-column gap-3 text-div justify-content-center">
+              <h1 className="display-3">Firdaus-Gate Model Schools</h1>
+              <div className="">
+                <p className="">
+                  Assalamu Alaikum! Welcome to Firdaus Gate Model Schools, where
+                  education meets faith. In our school, we nurture minds,
+                  hearts, and character. Explore the enriching blend of
+                  academics and Islamic values...
+                </p>
+              </div>
+              <div className="button-group d-flex flex-row">
+                <Button blue>Get started</Button>
+                <span> </span>
+                <Button white>About us</Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-md-8 right h-100">
-        <CarouselContainer>
-    </CarouselContainer>
+          <div className="col-md-8 right h-100">
+            <CarouselContainer></CarouselContainer>
+          </div>
         </div>
       </div>
-     </div>
-    <MobileImage>
-      </MobileImage> 
+      <MobileImage></MobileImage>
     </Wrapper>
   );
 }
 const Wrapper = styled.div`
-margin-top:70px;
+  margin-top: 70px;
 
-                  height: 100vh;
-                  color:white;
-.sticky{
-  margin-top:140px !important;
-}
-.row{
-  justify-content: space-between;
-align-items: center;
-
-}
-.left{
-  background-image: linear-gradient(to right, #00008b, #000000) !important;
-}
-.right{
-  background:  url('https://res.cloudinary.com/duvwweuhj/image/upload/v1700698715/Firdaus/IMG-20230901-WA0005_wsy9ri.jpg') !important;
-                  background-size: cover !important;
-                  background-repeat: no-repeat !important;
-                  background-position:center !important;
-}
-.text-div{
-  z-index: 99;
-h1{
-  width: 600px !important;
-}
-}
-.container{
-  height: 100%;
-  display: flex;
-justify-content: center;
-}
-.col-md-4{
-  
-}
-p{
-  font-size: 17px;
-}
-justify-content: left;
-align-items: start;
-text-align: left;
+  height: calc(100vh - 70px);
+  color: white;
+  .sticky {
+    margin-top: 140px !important;
+  }
+  .row {
+    justify-content: space-between;
+    align-items: center;
+  }
+  .left {
+    background-image: linear-gradient(to left, #00008b, #000000) !important;
+  }
+  .right {
+    background-image: url("https://res.cloudinary.com/duvwweuhj/image/upload/v1700698716/Firdaus/main-image.jpg") !important;
+    background-size: cover !important;
+    background-repeat: no-repeat !important;
+    background-position: center !important;
+  }
+  .text-div {
+    z-index: 99;
+    h1 {
+      width: 600px;
+      text-shadow: 0px 4px 3px rgba(0, 0, 0, 0.4),
+        0px 8px 13px rgba(0, 0, 0, 0.1), 0px 18px 23px rgba(0, 0, 0, 0.1);
+    }
+    p{
+      max-width: 400px !important;
+    }
+  }
+  .container {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .col-md-4 {
+  }
+  p {
+    font-size: 17px;
+  }
+  justify-content: left;
+  align-items: start;
+  text-align: left;
   .button-group {
     gap: 10px;
     justify-content: start !important;
     align-items: left;
   }
-  .image{
+  .image {
     max-width: 400px;
-    height:400px;
+    height: 400px;
     background-color: purple;
     border-radius: 30px;
   }
 
-  @media(max-width: 768px){
+  @media (max-width: 768px) {
     height: auto !important;
-    h1{
-        font-size: 48px;
+      .left {
+background: transparent !important;
+color: black;
+
+}
+h1 {
+  width: auto !important;
+      text-shadow: none !important;
+      font-size: 50px;
     }
-    p{
-      font-size: 16px;
+    p {
+      /* font-size: 16px; */
+      color: black;
+
     }
 
-  .text-div{
-    padding-bottom: 0 !important;
-  }
-  .container{
-    padding-bottom: 0 !important;
-}
+    .text-div {
+      padding-bottom: 0 !important;
+    }
+    .container {
+      padding-bottom: 0 !important;
+    }
   }
 `;
--- src/components/landing/gallery.js
+++ src/components/landing/gallery.js
@@ -1,111 +1,129 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import Marquee from "react-fast-marquee";
 import { Button } from "../custom/Button";
-export default function Gallery (){
-    const marqueImages = ["image1", "image2", "image3", "image4", "image5", "image6","image1", "image2", "image3", "image4", "image5", "image6"];
-    return (
-        <Container className="py-5">
-<div className="header d-flex flex-column justify-content-center align-items-center">
-    <h6>GALLERY</h6>
-    <h2>
-        View multiple photos that say alot about us and our top notch services
-    </h2>
-</div>
-<div className="d-flex flex-column body py-5 gap-4">
-
-<Marquee direction="left" speed={30} className="d-flex gap-3">
-    <div className="d-flex flex-row gap-3">
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    </div>
-</Marquee>
-<Marquee direction="left" speed={60} className="d-flex gap-3">
-    <div className="d-flex flex-row gap-3">
-    <div className="text">sports</div>
-    <div className="text">science</div>
-    <div className="text">art</div>
-    <div className="text">commercial</div>
-    <div className="text">debate</div>
-    <div className="text">culture</div>
-    <div className="text">excursions</div>
-    <div className="text">tech</div>
-    <div className="text">sports</div>
-    <div className="text">science</div>
-    <div className="text">art</div>
-    <div className="text">commercial</div>
-    <div className="text">debate</div>
-    <div className="text">culture</div>
-    <div className="text">excursions</div>
-    <div className="text">tech</div>
-    </div>
-</Marquee>
-<Marquee direction="left" speed={30} className="d-flex gap-3">
-    <div className="d-flex flex-row gap-3 ">
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    <div className="image"></div>
-    </div>
-</Marquee>
-</div>
-<div className="d-flex justify-content-center">
-
-<Button blue>View More</Button>
-</div>
-        </Container>
-    )
+export default function Gallery() {
+  const marqueImages1 = [
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698717/Firdaus/IMG-20230901-WA0010_wpqd5y.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698100/Firdaus/Screenshot_20221226-182710_1_jrfr32.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698093/Firdaus/Screenshot_20220823-083706_1_we5dio.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698090/Firdaus/Screenshot_20220823-083631_1_pqmhtr.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698087/Firdaus/Screenshot_20220823-083433_2_poesu0.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698086/Firdaus/Screenshot_20220823-083433_1_cne8do.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698084/Firdaus/Screenshot_20220822-213231_1_echgsu.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698084/Firdaus/Screenshot_20220822-213241_1_lxseka.jpg",
+  ];
+  const marqueImages2 = [
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698722/Firdaus/IMG-20230901-WA0012_zbvsdw.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698100/Firdaus/Screenshot_20221226-182710_1_jrfr32.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698724/Firdaus/IMG-20230901-WA0014_jur3yh.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698090/Firdaus/Screenshot_20220823-083631_1_pqmhtr.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698717/Firdaus/IMG-20230901-WA0009_1_h32p6p.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698086/Firdaus/Screenshot_20220823-083433_1_cne8do.jpg",
+    "https://res.cloudinary.com/duvwweuhj/image/upload/v1700698101/Firdaus/Screenshot_20221226-182739_1_za3qn6.jpg",
+  ];
+  return (
+    <Container className="py-5">
+      <div className="header d-flex flex-column justify-content-center align-items-center">
+        <h6>GALLERY</h6>
+        <h2>
+          View multiple photos that say alot about us and our top notch services
+        </h2>
+      </div>
+      <div className="d-flex flex-column body py-5 gap-4">
+        <Marquee direction="left" speed={30} className="d-flex gap-3">
+          <div className="d-flex flex-row gap-3">
+            {marqueImages1.map((image) => (
+              <div className="image">
+                <img src={image} />
+              </div>
+            ))}
+          </div>
+        </Marquee>
+        <Marquee direction="left" speed={60} className="d-flex gap-3">
+          <div className="d-flex flex-row gap-3">
+            <div className="text">sports</div>
+            <div className="text">science</div>
+            <div className="text">art</div>
+            <div className="text">commercial</div>
+            <div className="text">debate</div>
+            <div className="text">culture</div>
+            <div className="text">excursions</div>
+            <div className="text">tech</div>
+            <div className="text">sports</div>
+            <div className="text">science</div>
+            <div className="text">art</div>
+            <div className="text">commercial</div>
+            <div className="text">debate</div>
+            <div className="text">culture</div>
+            <div className="text">excursions</div>
+            <div className="text">tech</div>
+          </div>
+        </Marquee>
+        <Marquee direction="left" speed={30} className="d-flex gap-3">
+          <div className="d-flex flex-row gap-3 ">
+            {marqueImages2.map((image) => (
+              <div className="image">
+                <img src={image} />
+              </div>
+            ))}
+          </div>
+        </Marquee>
+      </div>
+      <div className="d-flex justify-content-center">
+        <Button blue>View More</Button>
+      </div>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-overflow: hidden !important;
-background-image: linear-gradient(to right, #00008b, #000000) !important;
-color: white;
-h6{
-    color:blue;
-}
-.image{
+  overflow: hidden !important;
+  background-image: linear-gradient(to right, #00008b, #000000) !important;
+  color: white;
+  h6 {
+    color: blue;
+  }
+  .image {
     width: 200px;
     height: 200px;
     background-color: purple;
     border-radius: 30px;
-}
-.text{
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: block;
+    }
+  }
+  .text {
     padding: 10px;
     background-color: #ffff;
     color: black;
     font-weight: 700;
-}
-.body{
+    border-radius: 10px;
+  }
+  .body {
     transform: rotate(-3deg);
-}
-.header{
-    h2{
-        font-size: 50px;
-        font-weight: 900;
-        text-align: center;
-        max-width: 700px;
+  }
+  .header {
+    h2 {
+      font-size: 50px;
+      font-weight: 900;
+      text-align: center;
+      max-width: 700px;
     }
-}
-@media screen and (max-width: 768px){
-    .header{
-        h2{
-            font-size: 36px;
-        }
+  }
+  @media screen and (max-width: 768px) {
+    .header {
+      h2 {
+        font-size: 36px;
+      }
     }
-    .image{
-    width: 150px;
-    height: 150px;
-    background-color: purple;
-}
-}
-`
+    .image {
+      width: 150px;
+      height: 150px;
+      background-color: purple;
+    }
+  }
+`;
