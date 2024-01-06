cddbd20dca9de78ff7c29929be6cf682cceb62c2
Ferinco <emmanuelferrum003@gmail.com>
2024-01-06 11:48:23 +0100

    function setup

---
+++ src/components/custom/teacherClass.js
@@ -0,0 +1,61 @@
+export function getTeacherClass(){
+        if(user.classHandled === ""){
+          setTeacherClass("JSS 1")
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("JSS 2")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("JSS 3")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("SSS 1")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("SSS 2")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("SSS 3")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Basic 1")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Basic 2")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Basic 3")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Basic 4")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Basic 5")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("K.G 1")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("K.G 2")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Nursery 1")
+    
+        }
+        else if (user.classHandled === ""){
+          setTeacherClass("Nursery 2")
+    
+        }
+}
