dd18545fda068d1c5f1da5da46b64b410db1fc0d
Ferinco <emmanuelferrum003@gmail.com>
2024-03-01 00:35:07 +0100

    admission-in-process in page

--- src/pages/admission/checkAdmission.js
+++ src/pages/admission/checkAdmission.js
@@ -1,95 +1,139 @@
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
+import { Icon } from "@iconify/react";
 export default function CheckAdmission() {
   const location = useLocation();
-  const referenceState = location.state;
-  console.log(referenceState);
+  const {
+    firstName,
+    classOfInterest,
+    surname,
+    parentEmailAddress,
+    reference,
+    transaction,
+    parentPhoneNumber,
+    gender,
+    presentClass,
+  } = location.state;
+
+  console.log(firstName);
   return (
-    <Wrapper className="container my-5">
-      <div className="header-section pb-2 d-flex flex-column justify-content-center align-items-center">
-      </div>
-      <div className="section">
-        <p className="sub-header">Admission Information</p>
-        <div className="row mt-3">
-          <div className="col-6">
+    <Wrapper className="container py-5">
+      <div className="content-div">
+        <div className="top-div p-2 w-100 d-flex flex-row mt-4 align-items-center gap-1 p-1">
+          <p className="m-0">
+            Your admission form has been successfully submitted to the school
+            management
+          </p>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
             <p>First Name</p>
-            <p className="item">{referenceState?.firstName}</p>
+            <h6>{firstName}</h6>
           </div>
-          <div className="col-6">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
             <p>Surname</p>
-            <p className="item">{referenceState?.surname}</p>
+            <h6>{surname}</h6>
           </div>
-          <div className="col-6 mt-2">
-            <p>Gender</p>
-            <p className="item">{referenceState?.gender}</p>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
+            <p>Parent Phone No.</p>
+            <h6>{parentPhoneNumber}</h6>
           </div>
-          <div className="col-6 mt-2">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
+            <p>Parent Email</p>
+            <h6>{parentEmailAddress}</h6>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
             <p>Present Class</p>
-            <p className="item">{referenceState?.presentClass}</p>
+            <h6>{presentClass}</h6>
           </div>
-          <div className="col-6 mt-2">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
             <p>Class Applied For</p>
-            <p className="item">{referenceState?.classOfInterest}</p>
+            <h6>{classOfInterest}</h6>
           </div>
-          <div className="col-6 mt-2">
-            <p> Phone Number</p>
-            <p className="item">{referenceState?.parentPhoneNumber}</p>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
+            <p>Reference ID</p>
+            <h6>{reference}</h6>
           </div>
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
+            <p>Transaction ID</p>
+            <h6>{transaction}</h6>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6 py-2 px-4 d-flex flex-column ">
+            <p>Admission Status</p>
+          </div>
+          <div className="col-md-6 py-2 px-4 d-flex flex-column">
+            <button disabled className="px-2 py-1">
+              Approved
+            </button>
+          </div>
+        </div>
+        <div className="end-div p-2 w-100 d-flex flex-row mt-4 align-items-center gap-1 p-1">
+          <Icon icon="lets-icons:info-alt-light" />
+
+          <p>Your admission has been aproved</p>
         </div>
       </div>
     </Wrapper>
   );
 }
 const Wrapper = styled.div`
-  background-image: url(/images/logo.png);
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  /* filter: blur(10px); */
-  position: relative;
-  &:before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(
-      255,
-      255,
-      255,
-      0.8
-    ); /* Adjust the alpha value for transparency */
+  .row mt-4 {
+    border-collapse: collapse !important;
   }
-  .header-section {
-    border-bottom: 1px solid #0d6efd;
-    position: relative;
+  p,
+  h6 {
+    margin: 0 !important;
   }
-  .sub-header {
-    background-color: #0d6efd;
-    color: white;
-    padding: 10px;
-    position: relative;
-    text-transform: capitalize !important;
-    \ span {
-      color: blue !important;
-    }
-    text-transform: uppercase;
+  p {
+    font-size: 14px;
   }
-  .row {
-    p {
-      margin: 0;
-      font-size: 15px;
-      font-weight: 400;
-    }
-  }
-
-  button {
-    position: relative;
-  }
-
-  .item {
-    font-weight: 500 !important;
+  h6 {
     text-transform: capitalize;
+    font-weight: 500 !important;
+  }
+  .end-div,
+  .top-div {
+    position: relative;
+    height: 50px;
+    background-color: #fbfbac;
+    padding: 8px !important;
+    p {
+      font-size: 12px;
+    }
+  }
+  button {
+    font-size: 12px;
+    color: green;
+    font-weight: 600;
+    width: fit-content;
+    border: 0 !important;
+    background-color: #9afd9a;
+    border-radius: 10px;
+  }
+  .content-div {
+    background-image: url(/images/logo.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    position: relative;
+    background-color: #f1f1f1;
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(255, 255, 255, 0.8);
+      
+    }
   }
 `;
--- src/pages/admission/continue.js
+++ src/pages/admission/continue.js
@@ -6,22 +6,30 @@ import { useState } from "react";
 import { PaymentService } from "../../services/paymentService";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import toast from "react-hot-toast";
 export default function ContinueAdmission() {
   const [referenceNo, setReferenceNo] = useState("");
   const [referenceState, setReferenceState] = useState({})
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
   console.log(referenceNo);
   const submitRef = async () => {
+    setLoading(true)
     try {
       const response = await axios.get(
         `https://ferrum-sever.onrender.com/api/confirmpayment/${referenceNo}`,
       );
       console.log(response);
       setReferenceState(response.data)
-      navigate("/admission/check-admission", { state: referenceState });
+      navigate("/admission/check-admission", { state: response.data });
+
 
     } catch (error) {
       console.error(error);
+      toast.error("Incorrect reference number, try again.")
+    }
+    finally{
+      setLoading(false)
     }
   };
   console.log(referenceState)
@@ -51,10 +59,19 @@ export default function ContinueAdmission() {
             onClick={() => {
               submitRef()
             }}
+            disabled={loading === true}
           >
+             {loading ? (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            ) : (
             <Link className="react-router-link" mto={PATH_PAGE.admissionForm}>
               Continue
             </Link>
+            )}
           </Button>
           <p className="mt-2 new-text">
             New Applicant ?{" "}
